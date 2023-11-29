dbfde1a4c3ed327afe19f36524c823863bb94769
F baffour <f.baffour@outlook.com>
2023-11-29 19:59:14 +0100

    first commit

---
+++ src/test/java/nl/novi/techiteasy/techiteasy1/TechitEasy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.techiteasy.techiteasy1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechitEasy1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
